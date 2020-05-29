1412cb8f51349ae14d4bc70c09c07e7186efaf91
marinhofilho <mariolemanf@gmail.com>
2020-05-29 00:23:49 -0300

    error with proptypes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import PropTypes from 'prop-types';
+import {Keyboard, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -18,17 +20,45 @@ import {
 } from './styles';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired(),
+  };
+
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({users: JSON.parse(users)});
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const {users} = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const {users, newUser} = this.state;
 
+    this.setState({loading: true});
+
     const response = await api.get(`/users/${newUser}`);
 
-    const {data} = {
+    const data = {
       name: response.data.name,
       login: response.data.login,
       bio: response.data.bio,
@@ -38,13 +68,20 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
+  handleNavigate = (user) => {
+    const {navigation} = this.props;
+
+    navigation.navigate('User', {user});
+  };
+
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
     return (
       <Container>
         <Form>
@@ -57,8 +94,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
 
@@ -70,7 +111,7 @@ export default class Main extends Component {
               <Avatar source={{uri: item.avatar}} />
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -80,7 +121,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
